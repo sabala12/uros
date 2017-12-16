25273f869dab65303af63bfa156e3af929cf7416
Eran Sabala <sabalah21@gmail.com>
2017-12-16 16:59:43 +0200

    refactor

--- src/arch/x86_64/boot/multiboot_header.asm
+++ src/arch/x86_64/boot/multiboot_header.asm
@@ -3,11 +3,10 @@
 
 section .multiboot_header
 header_start:
-    dd 0xe85250d6                ; magic number (multiboot 2)
-    dd flag_a | flag_b                    ; architecture 0 (protected mode i386)
-    dd header_end - header_start ; header length
-    ; checksum
-    dd 0x100000000 - (0xe85250d6 + 0 + (header_end - header_start))
+    dd 0xe85250d6							; magic number (multiboot 2)
+    dd flag_a | flag_b							; architecture 0 (protected mode i386)
+    dd header_end - header_start					; header length
+    dd 0x100000000 - (0xe85250d6 + 0 + (header_end - header_start))	; checksum
 
     ; insert optional multiboot tags here
 
